460d36aeb2903859b4a92f2026d9798923a6bd2a
tuan <tuandinh982002@gmail.com>
2021-05-10 13:56:04 +0700

    list-book-frontend

--- Source/angular/src/app/frontend/components/home/home.component.ts
+++ Source/angular/src/app/frontend/components/home/home.component.ts
@@ -11,41 +11,17 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class HomeComponent implements OnInit {
 
-  authors: any = [];
-  categories: any =[];
-  count = 0;
-  image_path = environment.image_url;
 
 
-  constructor(private authorService: AuthorService,
-    private categoryService: CategoryService,
-    private router: Router
 
+  constructor(
 
   ) { }
 
   ngOnInit(): void {
-    this.getAllAuthor();
-    this.getCategoryAll();
+   
   }
 
-  getAllAuthor() {
-    this.authorService.adminGetAllAuthor().subscribe(
-      (res) => {
-        this.authors = res;
-        // console.log(res)
-        // this.count = this.authors.length;
-      }
-    )
-  }
 
-  getCategoryAll() {
-    this.categoryService.adminGetAllCategory().subscribe(
-      (res) => {
-        this.categories = res;
-        // console.log(res)
-      }
-    )
-  }
 
 }
